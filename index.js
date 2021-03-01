3824f29895bc67ecbbb7908d1ad14adf70ea3737
SID-JET <76100309+SID-JET@users.noreply.github.com>
2021-03-01 13:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.querySelector(".big-heading").addEventListener("click", function(){var audio = new Audio("audio/lalypie.mp3");
 audio.play();
+animation();
 });
+
+function animation(){
+var a = document.querySelector(".big-heading");
+  a.classList.add("pressed");
+  setTimeout(function() {a.classList.remove("pressed")}, 100);
+}
